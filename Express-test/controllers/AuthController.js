79021e49851c3485c4db698f568b8e5c3dbbf2bc
Bryce-Du <63598874+Bryce-Du@users.noreply.github.com>
2023-03-19 08:29:00 -0500

    adjusted login function

--- Express-test/controllers/AuthController.js
+++ Express-test/controllers/AuthController.js
@@ -14,7 +14,7 @@ const login_post = async (req, res) => {
         const user = await User.login(email, password)
         const token = createToken(user._id)
         res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge*1000 })
-        res.status(200).json({ user: user._id})
+        res.status(200).json({ user: user._id, token })
         return
     } catch (err) {
         console.log(err.message, err.code);
